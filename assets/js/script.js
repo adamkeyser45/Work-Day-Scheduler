81b3cc0084d93bc50f1fc9f76274a51285e8cf62
Adam Keyser <65785770+adamkeyser45@users.noreply.github.com>
2020-07-03 09:19:48 -0500

    added clock to header

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,14 @@
 
-
+// Displays Current Date and Time below the header
 var currentDate = function() {
-    var date = "Hello!"
-    $("#currentDay").innerHTML = date;
+    var date = moment().format('MMMM Do YYYY, h:mm:ss a');
+    $("#currentDay").html(date);
 };
+
+// Updates the currentDate() function every second to act as a clock
+setInterval(function() {
+    currentDate();
+}, 1000);
+
+
+currentDate();
